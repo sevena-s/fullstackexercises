c5747fe16cf83f510eba64e4b1190cdfb8d47e09
sevena-s <sevenasandhu@gmail.com>
2021-08-18 02:23:29 -0700

    part 1.7

--- Part1/unicafe/src/App.js
+++ Part1/unicafe/src/App.js
@@ -1,25 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import React,{useState} from 'react';
+
+const Button = ({handleClick, text}) => (
+  <button onClick= {handleClick}>
+    {text}
+  </button>
+)
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [allScores, setScores] = useState(0)
+  const [avg, setAvg] = useState(0)
+
+  const handleGood = () => {
+    setGood(good+1)
+    setScores(allScores+1)
+    setAvg(avg+1)
+  }
+  const handleNeutral = () => {
+    setNeutral(neutral+1)
+    setScores(allScores+1)
+  }
+  const handleBad = () => {
+    setBad(bad+1)
+    setScores(allScores+1)
+    setAvg(avg-1)
+  }
+
+  return(
+    <div>
+      <h1>Give FeedBack</h1>
+      <Button handleClick={handleGood} text= {'Good'}/>
+      <Button handleClick={handleNeutral} text= {'Neutral'}/>
+      <Button handleClick={handleBad} text= {'Bad'}/>
+      <h1>Statistics</h1>
+      <p>Good {good}</p>
+      <p>Neutral {neutral}</p>
+      <p>Bad {bad}</p>
+      <p>All {allScores}</p>
+      <p>Average {avg/allScores}</p>
+      <p>Positive Percent {(good/allScores)*100} %</p>
     </div>
-  );
+  )
 }
 
 export default App;
