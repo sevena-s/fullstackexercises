14d8e9d7852b23504d99c9343be03311c67e676f
sevena-s <sevenasandhu@gmail.com>
2021-08-22 00:05:59 -0700

    2.6 Finished

--- Part2/phonebook/src/App.js
+++ Part2/phonebook/src/App.js
@@ -6,24 +6,49 @@ const App = () => {
       name: 'Arto Hellas'
     }
   ])
+
   const [newName, setNewName] = useState('')
+
+  const addNumber = (event) => {
+    event.preventDefault()
+    const numberObject = {
+      name: newName,
+      id: persons.length + 1
+    }
+    setPersons(persons.concat(numberObject))
+    setNewName('')
+  }
+
+  const handleNewNumber = (event) => {
+    setNewName(event.target.value)
+  }
 
   return(
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit= {addNumber}>
         <div>
-          name: <input />
+          name: <input 
+          value={newName}
+          onChange={handleNewNumber}/>
         </div>
         <div>
           <button type='submit'>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
+      {persons.map(number =>
+        <Numbers key={number.name} number ={number} /> )}
     </div>
   )
 
 
+}
+
+const Numbers = ({number}) => {
+  return(
+    <p>{number.name}</p>
+  )
 }
 
 export default App;
