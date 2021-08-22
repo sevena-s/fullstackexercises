7695b9b3cdd5254a686acfbbbe6d148a77f22356
sevena-s <sevenasandhu@gmail.com>
2021-08-21 22:10:30 -0700

    Finished 2.5

--- Part2/courseinfo2/src/App.js
+++ Part2/courseinfo2/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Course from "./Course";
 
 const App = () => {
   const course = [
@@ -44,8 +45,7 @@ const App = () => {
     }
   ]
 }
-  ]
-  //return <Course course={course}/>      
+  ]    
 
   return(
     <div>
@@ -60,48 +60,6 @@ const App = () => {
     </div>
   )
 
-}
-
-const Course = ({course}) => {
-  return(
-    <div>
-      <Header course={course.name}/>
-      <Content courses={course.parts}/>
-      <Total courses={course.parts}/>
-    </div>
-  )
-}
-
-const Header = (props) => {
-  return(
-    <div>
-      <h1>
-        {props.course}
-      </h1>
-    </div>
-  )
-}
-
-const Content = ({courses}) => {
-  return(
-    <div>
-        {courses.map((part,i) =>
-          <p key={i}>
-            {part.name} {part.ex}
-          </p>)}
-    </div>
-  )
-}
-
-const Total = ({courses}) => {
-  const parts2 = courses.map(part=>part.ex)
-  const total = parts2.reduce((s,p) => s += p)
-
-  return(
-    <div>
-      <p>Number of Exercises {total}</p>
-    </div>
-  )
 }
 
 export default App;
