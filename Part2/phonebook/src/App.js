dd1e87b6c6b015d5c559b54d275c6924e716bba4
sevena-s <sevenasandhu@gmail.com>
2021-08-24 01:40:19 -0600

    Added data to json server

--- Part2/phonebook/src/App.js
+++ Part2/phonebook/src/App.js
@@ -1,17 +1,32 @@
-import React,{useState} from 'react'
+import React,{useState, useEffect} from 'react'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
+  const [persons, setPersons] = useState([])
+/*  useState([
     { name: 'Arto Hellas', number: '040-123456' },
     { name: 'Ada Lovelace', number: '39-44-5323523' },
     { name: 'Dan Abramov', number: '12-43-234345' },
     { name: 'Mary Poppendieck', number: '39-23-6423122'}
   ])
+  */
 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newSearch, setSearch] = useState('')
   const [checker, setChecker] = useState(false)
+
+  const hook = () => {
+    console.log('effect')
+    axios 
+      .get('http://localhost:3001/persons')
+      .then(response =>{
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+
+  }
+  useEffect(hook, [])
 
   const addNumber = (event) => {
     event.preventDefault()
