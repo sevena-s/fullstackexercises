6ae0071382648733391da44e8090e1dd0f9c58b2
sevena-s <sevenasandhu@gmail.com>
2021-08-25 00:52:54 -0600

    Started working on Weather data from API

--- Part2/countries/src/App.js
+++ Part2/countries/src/App.js
@@ -2,11 +2,27 @@ import { useState, useEffect } from "react";
 import axios from 'axios'
 import { array } from "prop-types";
 
+const api_key = process.env.REACT_APP_API_KEY
+
 const App = () => {
 
   const [newSearch, setSearch] = useState('')
   const [countries, setCountries] = useState([])
   const [checker, setChecker] = useState(true)
+  const [weather, setWeather] = useState([])
+  const [capital, setCapital] = useState('New York')
+
+  const hookWeather = () => {
+    console.log('effect')
+    axios
+      .get(`http://api.weatherstack.com/current?access_key=${api_key}&query=${capital}`)
+      .then(response => {
+        console.log('promise fulfilled')
+        setWeather(response.data)
+      })
+  }
+  useEffect(hookWeather,[capital])
+
 
   const hook = () => {
     console.log('effect')
@@ -33,7 +49,6 @@ const App = () => {
   }
   console.log(newSearch)
   const array = (countries.filter(country => country.name.includes(newSearch)))
-  console.log(array)
 
   return(
   <div>
@@ -45,24 +60,37 @@ const App = () => {
       />
     </div>
   </form>
-    <CheckOneCountry checker= {checker} countries= {countries} newSearch= {newSearch} array= {array}/>
+    <CheckOneCountry checker= {checker} countries= {countries} newSearch= {newSearch} array= {array} weather= {weather}/>
   </div>
   )
 }
 
-const CheckOneCountry = ({checker, countries, newSearch, array}) => {
+const CheckOneCountry = ({checker, countries, newSearch, array, weather}) => {
   return(
     <div>
       {(array.length === 1) ?
-      (<OneCountryDisplay array= {array}/>)
-      :(<CheckCountryDisplay checker= {checker} countries= {countries} newSearch= {newSearch} array= {array}/>)}
+      (<OneCountryDisplay array= {array} weather= {weather}/>)
+      :(<CheckCountryDisplay checker= {checker} countries= {countries} newSearch= {newSearch} array= {array} weather= {weather}/>)}
     </div>
   )
 }
 
-const OneCountryDisplay = ({array}) => {
-  const flag = array.map(country => country.flag)
+const DisplayWeather = ({array, weather}) => {
+  const cap = array.map(country => country.capital)
+  const image = weather.current.weather_icons
 
+  return(
+    <div>
+      <h1>Weather in {cap}</h1>
+      <p>Temperature: {weather.current.temperature}</p>
+      <img src={image} style={{ height: 125, width: 125 }}/>
+    </div>
+  )
+}
+
+const OneCountryDisplay = ({array, weather}) => {
+  console.log(array)
+  const flag = array.map(country => country.flag)
   return(
     <div>
       <h1>{array.map(country => country.name)}</h1>
@@ -80,27 +108,52 @@ const OneCountryDisplay = ({array}) => {
       ))}
       <p></p>
       <img src={flag} style={{ height: 125, width: 125 }}/>
+      <p></p>
+      <DisplayWeather array= {array} weather= {weather}/>
     </div>
   )
 }
 
-const CheckCountryDisplay = ({checker, countries, newSearch, array}) => {
+const CheckCountryDisplay = ({checker, countries, newSearch, array, weather}) => {
   return(
     <div>
       {(array.length>10) ?
       <p>Too many countries!</p>
-      : (<CheckSearch checker= {checker} countries= {countries} newSearch= {newSearch}/>)}
+      : (
+      <CheckSearch checker= {checker} countries= {countries} newSearch= {newSearch} weather= {weather}/>
+      )}
+
     </div>
   )
 }
 
-const CheckSearch = ({checker, countries, newSearch}) => {
+const CheckSearch = ({checker, countries, newSearch, weather}) => {
   return(
   <div>
   {checker ? (countries.filter(country => country.name.includes(newSearch))).map(country => 
-    <p key={country.name}> {country.name}</p>) : countries.map(country =>
+    <p key={country.name}> {country.name} <Button array= {[country]} weather= {weather}/></p>) : countries.map(country =>
       <p key={country.name}> {country.name}</p>)}
   </div>
+  )
+}
+
+const Button = ({array, weather}) => {
+  const [showAll, setShowAll] = useState(false)
+
+  const name = array.map(country => country.capital)
+  console.log(weather)
+
+  return(
+    <div>
+    <button onClick={() => {
+      (setShowAll(!showAll));
+    }}>
+      show
+      {showAll ? 
+      <OneCountryDisplay array= {array} weather= {weather}/>
+      :null} 
+    </button>
+    </div>
   )
 }
 
