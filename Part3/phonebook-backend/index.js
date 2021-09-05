8c3247ae8a9c963621b7dbb741e9c87ea3eb0671
sevena-s <sevenasandhu@gmail.com>
2021-09-04 23:34:46 -0700

    Finished making app into a heroku app

--- Part3/phonebook-backend/index.js
+++ Part3/phonebook-backend/index.js
@@ -9,27 +9,28 @@ app.use(express.json())
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 app.use(cors())
+app.use(express.static('build'))
 
 let persons = [
   {
+    "id": 1,
     "name": "Arto Hellas",
-    "number": "040-123456",
-    "id": 1
+    "number": "040-123456"
   },
   {
+    "id": 2,
     "name": "Ada Lovelace",
-    "number": "39-44-5323523",
-    "id": 2
+    "number": "39-44-5323523"
   },
   {
+    "id": 3,
     "name": "Dan Abramov",
-    "number": "12-43-234345",
-    "id": 3
+    "number": "12-43-234345"
   },
   {
+    "id": 4,
     "name": "Mary Poppendieck",
-    "number": "39-23-6423122",
-    "id": 4
+    "number": "39-23-6423122"
   }
 ]
 
