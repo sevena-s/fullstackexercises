dcf532b9d7491d031ca455f848ae8af30a5b7ee3
sevena-s <sevenasandhu@gmail.com>
2021-09-04 22:44:40 -0700

    connected the backend and frontend

--- Part3/phonebook-backend/index.js
+++ Part3/phonebook-backend/index.js
@@ -11,33 +11,31 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 app.use(cors())
 
 let persons = [
-    [
-        { 
-          "id": 1,
-          "name": "Arto Hellas", 
-          "number": "040-123456"
-        },
-        { 
-          "id": 2,
-          "name": "Ada Lovelace", 
-          "number": "39-44-5323523"
-        },
-        { 
-          "id": 3,
-          "name": "Dan Abramov", 
-          "number": "12-43-234345"
-        },
-        { 
-          "id": 4,
-          "name": "Mary Poppendieck", 
-          "number": "39-23-6423122"
-        }
-    ]
+  {
+    "name": "Arto Hellas",
+    "number": "040-123456",
+    "id": 1
+  },
+  {
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523",
+    "id": 2
+  },
+  {
+    "name": "Dan Abramov",
+    "number": "12-43-234345",
+    "id": 3
+  },
+  {
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122",
+    "id": 4
+  }
 ]
 
 app.get('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
-    const person = persons[0].find(person => person.id === id)
+    const person = persons.find(person => person.id === id)
     if (person) {
         response.json(person)
       } else {
@@ -52,14 +50,14 @@ app.get('/api/persons', (request, response) => {
 
 app.get('/info', (request, response) => {
     response.send(
-        `<div>Phonebook has info for ${persons[0].length} people
+        `<div>Phonebook has info for ${persons.length} people
         <p>${new Date()} </p></div>`
         )
   })
 
 app.delete('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
-    persons = persons[0].filter(person => person.id !== id)
+    persons = persons.filter(person => person.id !== id)
     response.status(204).end()
   })  
 
