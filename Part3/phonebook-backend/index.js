35873372bced56903fd7365f77830ea8cd057211
sevena-s <sevenasandhu@gmail.com>
2021-09-16 15:30:15 -0700

    Backend now supports deleting and updating a Phone entry

--- Part3/phonebook-backend/index.js
+++ Part3/phonebook-backend/index.js
@@ -6,13 +6,23 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const PhoneBook = require('./models/phone')
 
-app.use(express.json())
+
 
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 app.use(cors())
 app.use(express.static('build'))
+app.use(express.json())
+
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message)
+
+  if(error.name === 'CastError') {
+    return response.status(400).send({error: 'malformatted id'})
+  }
+  next(error)
+}
 
 let persons = [
   {
@@ -37,15 +47,18 @@ let persons = [
   }
 ]
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-    if (person) {
+app.get('/api/persons/:id', (request, response, next) => {
+    PhoneBook.findById(request.params.id).then(person => {
+      if (person) {
         response.json(person)
       } else {
         response.status(404).end()
       }
-    
+    })
+    .catch(error => {
+      console.log("Error Id")
+      next(error)
+    })
   })
 
 app.get('/api/persons', (request, response) => {
@@ -61,10 +74,11 @@ app.get('/info', (request, response) => {
         )
   })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+    PhoneBook.findByIdAndRemove(request.params.id).then(person => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
   })  
 
 const generateID = () => {
@@ -92,12 +106,33 @@ app.post('/api/persons', (request, response) => {
         number: body.number,
       })
     
-      //persons = persons.concat(person)
-    
       person.save().then(savedPerson => {
         response.json(savedPerson)
       })
 })
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  PhoneBook.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+app.use(errorHandler)
 
 
 const PORT = process.env.PORT || 3001
